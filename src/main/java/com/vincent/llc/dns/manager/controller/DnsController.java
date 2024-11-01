09ac0aa7184c4fbdfb19a77b62686180d4a4e4e1
VincentB00 <vincentbui1999@gmail.com>
2024-11-01 11:40:29 -0400

    change to param

--- src/main/java/com/vincent/llc/dns/manager/controller/DnsController.java
+++ src/main/java/com/vincent/llc/dns/manager/controller/DnsController.java
@@ -10,7 +10,6 @@ import com.vincent.llc.dns.manager.service.DnsService;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,8 +28,8 @@ public class DnsController {
         return ResponseEntity.ok(records);
     }
 
-    @GetMapping("/nginx/certificates/{type}")
-    public ResponseEntity<?> getAllNginxCertificate(@PathVariable("type") String type) {
+    @GetMapping("/nginx/certificates")
+    public ResponseEntity<?> getAllNginxCertificate(@RequestParam String type) {
         return ResponseEntity.ok(this.dnsService.getAllNginxCertificate(type));
     }
 
